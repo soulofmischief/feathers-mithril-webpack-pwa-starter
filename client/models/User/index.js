a3ebe198aeb8ccfa610ddbe6a21dd1f2787987e5
soulofmischief <soulofmischief@protonmail.com>
2019-01-04 13:42:55 -0600

    Add User model

---
+++ client/models/User/index.js
@@ -0,0 +1,14 @@
+// @flow strict
+import { Model } from 'Models'
+//import { ctrl } from './controller'
+
+
+export const User = new class C_User extends Model {
+  constructor() {
+    super()
+  }
+
+  current = null
+
+  //ctrl = ctrl
+}
